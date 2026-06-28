11b1fecfdc78404aca6434f958fbf6b616a10270
Petar Dragicevic <dragicevicpetar14@gmail.com>
2026-06-28 10:56:05 +0200

    add init front project

---
+++ project/front/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { RaceControlPage } from './pages/race-control-page/race-control-page';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: RaceControlPage,
+  },
+];
---
+++ project/front/src/app/pages/race-control-page/race-control-page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-race-control-page',
+  templateUrl: './race-control-page.html',
+  styleUrl: './race-control-page.css',
+})
+export class RaceControlPage {}
